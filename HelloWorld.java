1f99a3d8646c06cf27f116ed3cbae15ecb5c2aaa
chetan-dsawant <chetans@nseit.com>
2019-11-16 14:28:44 +0530

    JAVA File COMMITED

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
-public class helloworld
+public class HelloWorld
 {
-	public saatic void main(String[] args)
+	public static void main(String[] args)
 	{
 		System.out.println("wlcom to jenkins java app");
 	}
